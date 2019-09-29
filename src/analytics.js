b530910967131479937bcc2d518b0c07b218386b
julien gatt <julien.gatt@hotmail.fr>
2019-09-29 15:30:30 +0800

    fill in functions to complete unit tests

--- src/analytics.js
+++ src/analytics.js
@@ -1,4 +1,4 @@
-import https from 'https'
+import axios from 'axios';
 
 /**
  * @property {string|{history: object[]}} source
@@ -14,7 +14,16 @@ export default class Analytics {
      * @returns {Promise<{history: object[]}>}
      */
     async getHistory() {
+        if(typeof this.source === 'object'){
+            return this.source;
+        }
+
+        return axios.get(this.source).then(response => response.data)
+        .catch(function (response) {
+            console.log(response);
+        });
     }
+
 
     /**
      * Get total amount of all validated purchases.
@@ -22,6 +31,14 @@ export default class Analytics {
      * @returns {Promise<number>}
      */
     async getTotalPurchasesAmount() {
+        const history = await this.getHistory()
+        .then(json => json.history);
+
+        const sumInCents = history
+        .filter(element => element.action === "buy" && element.details.validated)
+        .reduce((sum, { details: { amount } }) => sum + amount , 0);
+ 
+        return sumInCents / 100;
     }
 
     /**
@@ -30,5 +47,15 @@ export default class Analytics {
      * @returns {Promise<string[]>}
      */
     async getClients() {
+        const history = await this.getHistory()
+        .then(json => json.history);
+
+        const filter = history
+        .filter(element => element.action === "buy" && element.details.validated)
+        .map(element => element.user)
+        .filter((element, index, self) => self.indexOf(element) === index)
+        .reverse()
+
+        return filter;
     }
 }
